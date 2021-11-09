36daf59f2b2f8d81f6bcf08dace756b07ff89147
izzymiller <izzy@looker.com>
2021-11-08 16:27:46 -0800

    Create draw_hex.py

---
+++ draw_hex.py
@@ -0,0 +1,20 @@
+def draw_shape(num_sides):
+  if num_sides == 1:
+    print('-')
+  if num_sides == 2:
+    print('L')
+  if num_sides == 3:
+    print('∆')
+  if num_sides == 4:
+    print('□')
+  if num_sides == 5:
+    print('⬠')
+  if num_sides == 6:
+    print("""
+  _____
+ /     \ 
+/       \ 
+\       /
+ \_____/
+ \n
+ """)
